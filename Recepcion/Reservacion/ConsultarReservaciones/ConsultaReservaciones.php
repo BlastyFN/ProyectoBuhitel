65f773c80caab82b54d53d60d0912b8f453347ab
Blastjarna <eblastjarna@gmail.com>
2022-03-16 10:55:26 -0600

    Modificación del acomodo de carpetas de recepcion

--- Recepcion/Reservacion/ConsultarReservaciones/ConsultaReservaciones.php
+++ Recepcion/Reservacion/ConsultarReservaciones/ConsultaReservaciones.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reservaciones</title>
-    <link rel="stylesheet" href="../../design/styleCon.css">
+    <link rel="stylesheet" href="styleCon.css">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
--- Recepcion/Reservacion/EditarReservacion/EdicionReservacion.php
+++ Recepcion/Reservacion/EditarReservacion/EdicionReservacion.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edición Reservacion</title>
-    <link rel="stylesheet" href="../../design/styleR.css">
+    <link rel="stylesheet" href="../styleR.css">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
