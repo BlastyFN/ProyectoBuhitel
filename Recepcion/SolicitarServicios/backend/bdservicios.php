a82e59eb588424bd15094d3ebe784b8e3e82e3b3
Blastjarna <eblastjarna@gmail.com>
2022-05-17 12:35:50 -0500

    Minuscula

--- Recepcion/SolicitarServicios/backend/bdservicios.php
+++ Recepcion/SolicitarServicios/backend/bdservicios.php
@@ -2,7 +2,7 @@
     class database{
         private $con;
         function __construct(){
-            $this->con = new PDO ('mysql:host = localhost;dbname=corpo206_Buhitel','corpo206_gestorbuhi','ProyectoBuhitel2022');
+            $this->con = new PDO ('mysql:host = localhost;dbname=corpo206_buhitel','corpo206_gestorbuhi','ProyectoBuhitel2022');
         }
         //funciones
         
