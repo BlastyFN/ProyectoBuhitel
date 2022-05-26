36ba7faadd2b3ecc24e4b941df7e6927c1c52ec3
Blastjarna <eblastjarna@gmail.com>
2022-05-25 19:29:59 -0500

    Prueba con primer resultado de array

--- Huesped/sesion/verificarNumero.php
+++ Huesped/sesion/verificarNumero.php
@@ -4,8 +4,8 @@
         
         $String = "Hola, numero recibido ".$Numero;
         $resultado['registrado'] = json_encode($String);
-        // echo $resultado;
-        var_dump($resultado);
+        echo $resultado[0];
+        // var_dump($resultado);
         
     }
     else{
