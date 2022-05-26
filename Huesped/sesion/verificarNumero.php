e9fe6b76d9f98f1a5b993f869db49ba1b229d9b1
Blastjarna <eblastjarna@gmail.com>
2022-05-25 21:07:16 -0500

    Pruebas con array asociativo json

--- Huesped/sesion/verificarNumero.php
+++ Huesped/sesion/verificarNumero.php
@@ -1,15 +1,9 @@
 <?php 
     if (isset($_POST['numero'])) {
         $Numero = $_POST['numero'];
-        $resultado = [];
         $String = "Hola, numero recibido ".$Numero;
-        $reg['registrado'] = $String;
-
-        array_push($resultado, $reg);
-        $nuevoResultado = json_encode($resultado[0]);
-        echo $nuevoResultado[0];
-        // echo $resultado;
-        // var_dump($resultado);
+        $resultado = ["registrado"=>$String];
+        print_r(json_encode($resultado));
         
     }
     else{
