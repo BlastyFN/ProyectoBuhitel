ea8f93a2e73f08c7cd5c48858b945752fd93aa93
Isaac <isaacdiaztrejo@gmail.com>
2022-08-23 09:37:14 -0500

    añadir seguimiento de reportes a personal

--- Administrador/moduloReportesUsuarios/backend/bd.php
+++ Administrador/moduloReportesUsuarios/backend/bd.php
@@ -5,8 +5,7 @@ class database
 	private $con;
 
 	function __construct(){
-		$this->con = new PDO ('mysql:host localhost= localhost;dbname=corpo206_buhitel','root','');
-	}
+		$this->con = new PDO ('mysql:host = localhost;dbname=corpo206_buhitel','corpo206_gestorbuhi','ProyectoBuhitel2022');	}
 
 	function obtenerListaPersonal($tipo){
 		$sql = $this->con->prepare("SELECT * FROM personal WHERE Personal_Tipo = '".$tipo."'");
--- General/SolucionDeReportes/BackendReportes/obtenerReportes.php
+++ General/SolucionDeReportes/BackendReportes/obtenerReportes.php
@@ -1,8 +1,8 @@
 <?php
     include "bd.php";
     session_start();
-    //$personal =  $_SESSION['sesionPersonal']['ID'];
-	$personal = 40;
+    $personal =  $_SESSION['sesionPersonal']['ID'];
+	
     $bd = new database();
     $res = $bd-> obtenerReportes($personal);
     echo json_encode($res);
