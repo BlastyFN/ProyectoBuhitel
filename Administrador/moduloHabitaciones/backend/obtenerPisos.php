ebded702b927515c8a9e60b8969063905b30b6a5
Isaac <isaacdiaztrejo@gmail.com>
2022-10-04 01:33:12 -0500

    prueba modificacion habs

--- Administrador/moduloHabitaciones/backend/obtenerPisos.php
+++ Administrador/moduloHabitaciones/backend/obtenerPisos.php
@@ -5,7 +5,7 @@ session_start();
 	    $bd = new database();
         $pisos = $bd-> obtenerPisos($hotel);
         foreach ($pisos as $piso) {
-            $habs = $bd->obtenerHabs($hotel,$piso);
+            $habs = $bd->obtenerHabs($hotel, $piso['piso_ID']);
             $piso->habs = $habs;
         }
         echo json_encode($pisos); 
