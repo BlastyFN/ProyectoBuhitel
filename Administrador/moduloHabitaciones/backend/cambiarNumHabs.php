cc4a7aa2e3d8d696299a06273adc14d3aff02d65
IsaacOQ <isaacdiaztrejo@gmail.com>
2022-10-11 01:06:58 -0500

    Update cambiarNumHabs.php

--- Administrador/moduloHabitaciones/backend/cambiarNumHabs.php
+++ Administrador/moduloHabitaciones/backend/cambiarNumHabs.php
@@ -7,27 +7,29 @@
         // $pisoID = $_POST['pisoID'];
         $hotel = $_SESSION['sesionPersonal']['Hotel'];
         $bd = new database();
-    
         
-        for($contPisos = 0; $contPisos < count($antiguoNumHabs); $contPisos++){
-            $antiguoNumHabs = ($contPisos+1) * 100 + (int)$antiguoNumHabs;
-            $nuevoNumHabs =($contPisos+1) * 100 + (int)$nuevoNumHabs;
+        
+        for($contPisos = 0; $contPisos < count($nuevoNumHabs); $contPisos++){
+            $antiguoNum = ($contPisos+1) * 100 + (int)$antiguoNumHabs[$contPisos];
+            $nuevoNum =($contPisos+1) * 100 + (int)$nuevoNumHabs[$contPisos];
+            
             $pisoID = $bd-> obtenerPisoID(($contPisos+1),$hotel);
             $tipoHab = $bd->obtenerPrimerTipoHabID($hotel);
-            if($nuevoNumHabs > $antiguoNumHabs){
-                for($cont = $antiguoNumHabs; $cont <= $nuevoNumHabs; $cont++){
+
+            if($nuevoNum > $antiguoNum){
+                for($cont = $antiguoNum; $cont <= $nuevoNum; $cont++){
                     
                         $bd->registrarHab($cont,$pisoID,$tipoHab);
                     
                 }
-                echo "Se han agregado ". ($nuevoNumHabs - $antiguoNumHabs). " habitaciones";
+                echo "Se han agregado ". ($nuevoNum - $antiguoNum). " habitaciones";
             } else{
-                for($cont = $antiguoNumHabs; $cont > $nuevoNumHabs; $cont--){
+                for($cont = $antiguoNum; $cont > $nuevoNum; $cont--){
                     
                         $bd->eliminarHab((string)$cont,$hotel);
                     
                 }
-                echo "Se han eliminado ". ($antiguoNumHabs - $nuevoNumHabs). " habitaciones";
+                echo "Se han eliminado ". $antiguoNum - $nuevoNum. " habitaciones";
             }
 
         }
