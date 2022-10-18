4a5505710cdca1d6ba0304fff7008b1b6a740860
IsaacOQ <isaacdiaztrejo@gmail.com>
2022-10-18 09:37:39 -0500

    Update obtenerTiempoOcupaciones.php

--- Administrador/moduloReportesSistema/backendReportesSistema/obtenerTiempoOcupaciones.php
+++ Administrador/moduloReportesSistema/backendReportesSistema/obtenerTiempoOcupaciones.php
@@ -5,9 +5,9 @@ session_start();
     $bd = new database();
     $hotel = $_SESSION['sesionPersonal']['Hotel'];
     $condicionalHabs = $_POST['habs'];
-    for($i = 0; $i < 14; $i+=2){
+    for($i = 2; $i <= 14; $i+=2){
         $resConsulta = 0;
-        $resConsulta = $bd->obtenerTiempoOcupaciones($hotel, $i,$i+2, $condicionalHabs);
+        $resConsulta = $bd->obtenerTiempoOcupaciones($hotel, $i, $condicionalHabs);
         array_push($arregloRes, $resConsulta);
     }
     
